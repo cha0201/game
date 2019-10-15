fe4573846d12c207859bd8a78c21cad9162ea021
cha0201 <171477381@qq.com>
2019-10-15 17:32:02 +0800

    modify: 查询统计修改

--- src/main/java/com/example/game/service/impl/GameRecordServiceImpl.java
+++ src/main/java/com/example/game/service/impl/GameRecordServiceImpl.java
@@ -10,9 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -39,8 +39,9 @@ public class GameRecordServiceImpl implements GameRecordService {
     public List<GameRecordStatistics> getRecordStatistics(String time, String account, String operator) {
         Date startDateTime = null;
         Date endDateTime = new Date();
+        String timeSql=" record_date as date,";
         if (!StringUtils.isEmpty(time)) {
-            DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
             LocalDateTime inputDate = LocalDateTime.now();
             // 0本周，1下周，-1上周
             if ("1".equals(time)) {
@@ -53,18 +54,30 @@ public class GameRecordServiceImpl implements GameRecordService {
                 //本月
                 startDateTime= Date.from(DateUtils.monthStart(0).atZone(ZoneOffset.ofHours(8)).toInstant());
             }
+            timeSql=" concat('" +sdf.format(startDateTime)+"'"+",'~','"+sdf.format(endDateTime)+"') as date,";
+        }
+        if(StringUtils.isEmpty(account)&&StringUtils.isEmpty(operator)){
+            timeSql=" record_date as date,";
         }
 
         String columnSql=null;
         String accountSql=" record_account as account, ";
         String operatorSql=" (select user_nick_name from game_user where game_user_id=game_record.game_user_id) as operator ";
         StringBuffer columnCondition=new StringBuffer();
-        String columnCondition1=" record_account as account, 1 as pourNum, record_amount as pourAmount, record_valid_amount as effectAmount, record_gain profitAmount, (select user_nick_name from game_user where game_user_id=game_record.game_user_id) as operator " ;
+        String columnCondition1=timeSql+" record_account as account, 1 as pourNum, record_amount as pourAmount, record_valid_amount as effectAmount, record_gain profitAmount, (select user_nick_name from game_user where game_user_id=game_record.game_user_id) as operator " ;
         if(StringUtils.isEmpty(account)&&!StringUtils.isEmpty(operator)){
             accountSql=" '' as account, ";
         }else if(!StringUtils.isEmpty(account)&&StringUtils.isEmpty(operator)){
             operatorSql= " '' as operator ";
         }
+        if(!StringUtils.isEmpty(account)||!StringUtils.isEmpty(operator)){
+            if(StringUtils.isEmpty(time)){
+                timeSql=" '' as date,";
+            }
+        }
+
+
+        columnCondition.append(timeSql);
         columnCondition.append(accountSql);
         columnCondition.append(" count(id) as pourNum, ");
         columnCondition.append(" sum(record_amount) as pourAmount, sum(record_valid_amount) as effectAmount,sum(record_gain) profitAmount,");
